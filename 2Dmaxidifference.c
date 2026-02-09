97bb557bf451c6d7a04c8143657e4d8fb5ab7dd8
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-02-09 22:21:47 +0530

    Add files via upload

---
+++ 2Dmaxidifference.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int r, c, i, j;
+    int a[10][10];
+    int sum[10], maxDiff = 0, diff;
+    scanf("%d %d", &r, &c);
+    for (i = 0; i < r; i++) {
+        sum[i] = 0;
+        for (j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+            sum[i] = sum[i] + a[i][j];
+        }
+    }
+    for (i = 0; i < r; i++) {
+        for (j = i + 1; j < r; j++) {
+            diff = sum[i] - sum[j];
+            if (diff < 0)
+                diff = -diff;
+            if (diff > maxDiff)
+                maxDiff = diff;
+        }
+    }
+    printf("%d", maxDiff);
+    return 0;
+}
